3b2695a9566828ff6a46a2746058ec8ada719149
diwa <diwa@diwas-MacBook-Air.local>
2024-11-04 14:44:55 +0100

    créate run() fn

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("hello world");
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+async function Run() {
+    const patToken = (0, core_1.getInput)("organization-token");
+    const apiBaseUrl = (0, core_1.getInput)("api-url");
+    console.log(`hello world avec comme arg cette api url -> ${apiBaseUrl}`);
+}
+(async () => await Run())();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function Run() {
+    const patToken = getInput("organization-token");
+    const apiBaseUrl = getInput("api-url");
+
+    console.log(`hello world avec comme arg cette api url -> ${apiBaseUrl}`);
+}
+
+(async () => await Run())();
